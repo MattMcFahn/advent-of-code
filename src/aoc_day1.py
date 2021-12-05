71ac2dab3e679039a7e01b3e143c791c6a9df3f5
Matt McFahn <matthew.mcfahn@bjss.com>
2021-12-05 17:58:41 +0000

    feat, update with missing solutions for day 4 and 1

---
+++ src/aoc_day1.py
@@ -0,0 +1,51 @@
+"""Solutions for day 1"""
+from typing import List
+
+
+def get_input(filepath: str) -> List[int]:
+    """Simple helper to get useful data structures from input"""
+    with open(filepath) as file:
+        lines = file.readlines()
+
+    return lines
+
+
+def make_sliding_window(input_values: List) -> List:
+    """Helper that takes a list of length n and returns a list of length (n-3),
+     where element i = sum ( elem_i, elem_(i-1), elem_(i-2) )
+    """
+    summed_values = [
+        input_values[i] +
+        input_values[i-1] +
+        input_values[i-2]
+        for i in range(2, len(input_values))
+    ]
+    return summed_values
+
+
+def get_increase_count(input_values: List) -> int:
+    """Counts the number of increases between corresponding entries in the list of input_values"""
+    counter = 0
+
+    previous = input_values.pop(0)
+    while input_values:
+        current = input_values.pop(0)
+        if current > previous:
+            counter += 1
+        previous = current
+
+    return counter
+
+
+if __name__ == "__main__":
+    FILEPATH = r"/resources/aoc-day1.txt"
+    values = get_input(FILEPATH)
+
+    # Part 1, increase count on values
+    challenge_one = get_increase_count(values)
+    print(f"Challenge one: {challenge_one}")
+
+    # Part 2, increase count on window values
+    window_values = make_sliding_window(values)
+    window_result = get_increase_count(window_values)
+    print(f"Challenge two: {window_result}")
--- src/aoc_day4.py
+++ src/aoc_day4.py
@@ -1,60 +1,57 @@
 """A python solution to Advent of Code day 4 challenges"""
-from typing import Union, Dict, List
+from typing import Tuple, Dict, List
 
 import pandas as pd
 import numpy as np
 
-# pylint: disable=fixme
-# TODO: Update with the working version from other machine
-# TODO: Add input file
 
-
-def setup_game(filepath: str) -> Union[List, Dict[int, pd.DataFrame]]:
+def setup_game(filepath: str) -> Tuple[List, Dict[int, pd.DataFrame]]:
     """
     From the input filepath, creates a list of integers to be called in order,
     and the three boards for the game
-
     """
     with open(filepath) as file:
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
 
-    numbers = [int(x) for x in lines[0].split(",")]
+    input_numbers = [int(x) for x in lines[0].split(",")]
     lines = lines[2:]
 
-    boards = {}
-    for i in [1, 2, 3]:
+    input_boards = {}
+    i = 0
+    while len(lines) > 0:
+        i += 1
         relevant_lines = [[int(x) for x in y.split(" ") if x != ""] for y in lines[:5]]
         board = pd.DataFrame({i: relevant_lines[i] for i in range(0, 5)}).T
-        boards[i] = board
+        input_boards[i] = board
         lines = lines[6:]
 
-    return numbers, boards
+    return input_numbers, input_boards
 
 
-def check_verticals(board, numbers):
+def check_verticals(board, input_numbers):
     """Implements the vertical checks of check_board_won"""
-    for i in range(0, len(board)):  # pylint: disable=consider-using-enumerate
+    for i in range(0, len(board)):
         values = set(board[i].values)
-        if not values - set(numbers):
+        if not values - set(input_numbers):
             return True
     return False
 
 
-def check_horizontals(board, numbers):
+def check_horizontals(board, input_numbers):
     """Implements the horizontal checks of check_board_won"""
-    return check_verticals(board.T, numbers)
+    return check_verticals(board.T, input_numbers)
 
 
-def check_diagonals(board, numbers):
+def check_diagonals(board, input_numbers):
     """Implements the diagonal checks of check_board_won"""
     diag_one = set(np.diag(board))
     diag_two = set(np.diag(board))
 
-    return not diag_one - set(numbers) or not diag_two - set(numbers)
+    return not diag_one - set(input_numbers) or not diag_two - set(input_numbers)
 
 
-def check_board_won(board, numbers):
+def check_board_won(board, input_numbers):
     """Checks whether a selection of numbers (>5) completes a board.
     Rules that ensure a board will be complete:
         * A diagonal line
@@ -62,27 +59,79 @@ def check_board_won(board, numbers):
         * A vertical line
     """
     return (
-        check_verticals(board, numbers)
-        or check_horizontals(board, numbers)
-        or check_diagonals(board, numbers)  # pylint: disable=line-too-long
+        check_verticals(board, input_numbers)
+        or check_horizontals(board, input_numbers)
+        or check_diagonals(board, input_numbers)
     )
 
 
-def get_winning_board(boards, numbers):
-    """Returns winning board and last number added to win"""
+def get_winning_board(input_boards: Dict[int, pd.DataFrame], input_numbers: List[int]) -> Tuple[pd.DataFrame, List]:
+    """Returns winning board, and the numbers called up till that point"""
     i = 5
-    input_length = len(numbers)
+    input_length = len(input_numbers)
+
     while i <= input_length:
-        cut_numbers = numbers[:i]
-        for board in boards.values():
-            if check_board_won(board, numbers):
+        cut_numbers = input_numbers[:i]
+        for board in input_boards.values():
+            if check_board_won(board, cut_numbers):
                 winning_board = board
-                winning_number = cut_numbers[-1]
+                return winning_board, cut_numbers
         i += 1
-    return winning_board, winning_number
+    return Exception("No winning board found - check code")
+
+
+def get_losing_board(input_boards: Dict[int, pd.DataFrame], input_numbers: List[int]) -> Tuple[pd.DataFrame, List]:
+    """Returns the LAST board that would win, and the numbers called up till that point"""
+    input_length = len(input_numbers)
+    i = len(input_numbers)
+
+    while i >= 5:
+        cut_numbers = input_numbers[:i]
+        for board in input_boards.values():
+            if not check_board_won(board, cut_numbers):
+                losing_board = board
+                return losing_board, input_numbers[: i + 1]
+        i -= 1
+    return Exception("No losing board found - check code")
+
+
+def get_unmarked_number_total(winning_board: pd.DataFrame, input_numbers: List):
+    """Returns the sum of all numbers on winning_board that aren't in numbers"""
+    values = []
+    for x in winning_board.columns:
+        values.extend([y for y in winning_board[x].to_list()])
+
+    non_marked_number_set = set(values) - set(input_numbers)
+    non_marked_numbers = [x for x in values if x in non_marked_number_set]
+    return sum(non_marked_numbers)
+
+
+def challenge_one(input_numbers: List, input_boards: Dict[int, pd.DataFrame]):
+    """Chain steps for challenge 1 - get the winning board, and have sum of unmarked numbers * winning number"""
+    winning_board, winning_numbers = get_winning_board(input_boards, input_numbers)
+    winning_number = winning_numbers[-1]
+
+    unmarked_number_total = get_unmarked_number_total(winning_board, winning_numbers)
+    result = unmarked_number_total * winning_number
+    return result
+
+
+def challenge_two(input_numbers: List, input_boards: Dict[int, pd.DataFrame]):
+    """Chain steps for challenge 2 - get the LAST board to win, and sum unmarked numbers * winning number"""
+    losing_board, losing_numbers = get_losing_board(input_boards, input_numbers)
+    winning_number = losing_numbers[-1]
+
+    unmarked_number_total = get_unmarked_number_total(losing_board, losing_numbers)
+    result = unmarked_number_total * winning_number
+    return result
 
 
 if __name__ == "__main__":
-    FILEPATH = r"../resources/aoc-day4-TEST.txt"
-    input_numbers, input_boards = setup_game(FILEPATH)
-    final_board, final_number = get_winning_board(input_boards, input_numbers)
+    FILEPATH = r"/resources/aoc-day4.txt"
+    numbers, boards = setup_game(FILEPATH)
+
+    challenge_one_result = challenge_one(numbers, boards)
+    print(f"Challenge one: {challenge_one_result}")
+
+    challenge_two_result = challenge_two(numbers, boards)
+    print(f"Challenge two: {challenge_two_result}")
