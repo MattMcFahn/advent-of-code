fa98efc1415c4024f413c33000780a3be312dd28
Matt McFahn <mattmcfahn161@hotmail.co.uk>
2021-12-18 19:14:07 +0000

    feat, day10, part 2

--- src/aoc_day10.py
+++ src/aoc_day10.py
@@ -23,10 +23,11 @@ def setup_game(filepath: str) -> List[List[str]]:
     return lines
 
 
-def get_broken_strings(lines):
-    """"""
+def get_broken_and_incomplete_strings(lines):
+    """Main logic for identifying which strings are broken / incomplete"""
     invalid_strings = []
     breaking_chars = []
+    incomplete_strings = []
 
     for line in lines:
         original_line = line
@@ -38,16 +39,38 @@ def get_broken_strings(lines):
 
             invalid_matches = re.search(closing_parens, line)
             breaking_chars.append(invalid_matches.group(0))
+        else:
+            incomplete_strings.append(line)
 
-    return invalid_strings, breaking_chars
+    return invalid_strings, breaking_chars, incomplete_strings
 
 
 def challenge_one(lines):
     """Assumes lines are of even length"""
-    breaking_chars = get_broken_strings(lines)[1]
+    breaking_chars = get_broken_and_incomplete_strings(lines)[1]
     scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
     total = sum([scores[x] for x in breaking_chars])
     return total
+
+
+def challenge_two(lines):
+    """Same assumption as challenge one"""
+    incomplete_strings = get_broken_and_incomplete_strings(lines)[2]
+    completions = []
+    for string in incomplete_strings:
+        completion = "".join(get_closing_paren(x) for x in string)[::-1]
+        completions.append(completion)
+
+    score_map = {")": 1, "]": 2, "}": 3, ">": 4}
+    scores = []
+    for string in completions:
+        score = 0
+        for char in string:
+            score *= 5
+            score += score_map[char]
+        scores.append(score)
+    scores.sort()
+    return scores[int((len(scores) - 1) / 2)]
 
 
 if __name__ == "__main__":
@@ -57,3 +80,7 @@ if __name__ == "__main__":
     # Challenge one
     challenge_one = challenge_one(input_lines)
     print(f"Challenge one: {challenge_one}")
+
+    # Challenge two
+    challenge_two = challenge_two(input_lines)
+    print(f"Challenge two: {challenge_two}")
